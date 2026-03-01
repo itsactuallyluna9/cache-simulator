ee7b6024ee058f1809fbd609ebd1d2546e612290
169411831 <ckawase@users.noreply.github.com>
2026-03-01 17:01:11 -0600

    Implement write-back

--- src/cache_simulator/cache.py
+++ src/cache_simulator/cache.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from address import apply_address_format
 
 @dataclass
@@ -9,6 +9,7 @@ class CacheLine:
     access_counter: int = 0 # update when hit
     used_timestamp: int = 0 # update when hit and swapped
     swapped_timestamp: int = 0 # update when swapped
+    written_offsets: dict = field(default_factory=dict) # holds the count of write time depending on offset
 
 class Cache:
     cache: list[list[CacheLine]]
@@ -22,15 +23,15 @@ class Cache:
                 line_set.append(CacheLine())
             self.cache.append(line_set)
 
-    def check(self, tag_bits: str, set_bits: str, clock: int)->bool:
+    def check(self, tag_bits: str, set_bits: str, clock: int)->CacheLine:
         if set_bits == "": # fully-associative
             for set in self.cache:
                 for line in set:
                     if line.tag == tag_bits and not line.invalid:
                         line.access_counter += 1
                         line.used_timestamp = clock
-                        return True
-            return False
+                        return line
+            return None
         else:
             set_index = int(set_bits, 2)
             set = self.cache[set_index]
@@ -38,5 +39,5 @@ class Cache:
                 if line.tag == tag_bits and not line.invalid:
                     line.access_counter += 1
                     line.used_timestamp = clock
-                    return True
-            return False     
+                    return line
+            return None
--- src/cache_simulator/simulation.py
+++ src/cache_simulator/simulation.py
@@ -2,20 +2,22 @@ from memory import Memory
 from cache import Cache, CacheLine
 from address import apply_address_format
 from instructions import rng_instructions
+from writes import write_through, write_back, flush_line
 from replacements import swap_page, random_replacement,least_frequently_used, least_recently_used, first_in_first_out
 
 class SimulationEngine:
-    mapping_strategy = ["direct", "fully-associative", "set-associative"]
+    mapping_strategy = ["direct", "fully_associative", "set_associative"]
     replacement_algorithum = ["FIFO", "LRU", "LFU", "random"]
+    write_policy = ["write_back", "write_through"]
 
-    def __init__(self, memory_size: int, page_size: int, cache_size: int, num_instruction: int, mapping: str, replacement: str= None, line_per_set: int=1):
+    def __init__(self, memory_size: int, page_size: int, cache_size: int, num_instruction: int, mapping: str, write_policy : str,replacement: str= None, line_per_set: int=1):
         if mapping == "direct":
             self._num_set = cache_size // page_size
             self._line_per_set = 1
-        elif mapping == "fully-associative":
+        elif mapping == "fully_associative":
             self._num_set = 1
             self._line_per_set = cache_size // page_size
-        elif mapping == "set-associative":
+        elif mapping == "set_associative":
             self._num_set = (cache_size // page_size) // line_per_set
             self._line_per_set = line_per_set
         else:
@@ -25,50 +27,69 @@ class SimulationEngine:
             raise AttributeError("Replacement algorithm cannot be used with direct mapping")
         if replacement is not None and replacement not in self.replacement_algorithum:
             raise AttributeError(f"Invalid replacement algorithm is given: {replacement}")
+        
+        if write_policy not in self.write_policy:
+            raise ArithmeticError(f"Invalid write policy is given : {write_policy}")
         self._memory_size = memory_size
         self._page_size = page_size
         self._cache_size = cache_size
         self._mapping = mapping
         self._replacement = replacement
+        self._write_policy = write_policy
         self._num_instruction = num_instruction
         self._memory = Memory(self._memory_size, self._page_size)
         self._cache = Cache(self._cache_size, self._page_size, self._num_set, self._line_per_set)
-        self._hit_counter = 0
+        self._read_hit = 0
+        self._read_miss = 0
+        self._write_hit = 0
+        self._write_miss = 0
 
     def simulate(self):
         clock = 0
         for instruction, addr in rng_instructions(self._num_instruction, self._memory_size):
-            tag_bits, line_bits, offset_bits = apply_address_format(addr, self._memory_size, self._page_size, self._cache_size, self._line_per_set)
+            tag_bits, set_bits, offset_bits = apply_address_format(addr, self._memory_size, self._page_size, self._cache_size, self._line_per_set)
+            page_bits = tag_bits + set_bits
+            page_index = int(page_bits, 2)
+            line = self._cache.check(tag_bits, set_bits, clock)
             if instruction == "r":
-                self._read_cache(tag_bits, line_bits, clock)
+                if line is not None:
+                    self._read_hit += 1
+                else:
+                    self._read_miss += 1
+                    self._handle_miss(tag_bits, set_bits, clock, page_index)
             elif instruction == "w":
-                pass
+                if line is not None:
+                    self._write_hit += 1
+                else:
+                    self._write_miss += 1
+                    line = self._handle_miss(tag_bits, set_bits, clock, page_index)
+                self._write_cache(line, offset_bits, page_index)
             else:
-                raise ValueError(f"Invalid instruction : {instruction}")
+                raise ValueError(f"Invalid instruction: {instruction}")
             clock += 1
-        print(f"Hit number : {self._hit_counter}")
-        print(f"Missed number: {self._num_instruction - self._hit_counter}")
+        print(f"Read hits: {self._read_hit}, misses: {self._read_miss}")
+        print(f"Write hits: {self._write_hit}, misses: {self._write_miss}")
 
-    def _read_cache(self, tag_bits: str, line_bits: str, clock: int):
-        if self._cache.check(tag_bits, line_bits, clock):
-            self._hit_counter += 1
+    def _handle_miss(self, tag_bits: str, set_bits: str, clock: int, page_index: int):
+        if self._mapping == "direct":
+            set_index=int(set_bits, 2)
+            line = swap_page(self._cache, tag_bits, set_index=set_index, line_index=0, clock=clock)
+            flush_line(self._memory, line, page_index)
         else:
-            if self._mapping == "direct":
-                set_index=int(line_bits, 2)
-                swap_page(self._cache, tag_bits, set_index=set_index, line_index=0, clock=clock)
-            else:
-                if self._mapping == "fully-associative":
-                    set_index = 0
-                elif self._mapping == "set-associative":
-                    set_index = int(line_bits, 2)
-                self._do_replacement(tag_bits, set_index, clock)
-
+            if self._mapping == "fully_associative":
+                set_index = 0
+            elif self._mapping == "set_associative":
+                set_index = int(set_bits, 2)
+            line = self._do_replacement(tag_bits, set_index, clock)
+            flush_line(self._memory, line, page_index)
+        return line
+    
     def _do_replacement(self, tag_bits: str, set_index, clock:int):
         victim_set = self._cache.cache[set_index]
         for index, line in enumerate(victim_set):
             if line.invalid:
-                swap_page(self._cache, tag_bits, set_index=set_index, line_index=index, clock=clock)
-                return
+                line = swap_page(self._cache, tag_bits, set_index=set_index, line_index=index, clock=clock)
+                return line
         if self._replacement == "random":
             victim_index = random_replacement(victim_set)
         elif self._replacement == "LFU":
@@ -80,4 +101,12 @@ class SimulationEngine:
         else:
             raise ValueError(f"Unknown replacement policy : {self._replacement}")
         
-        swap_page(self._cache, tag_bits, set_index=set_index, line_index=victim_index, clock=clock)
+        line = swap_page(self._cache, tag_bits, set_index=set_index, line_index=victim_index, clock=clock)
+
+        return line
+
+    def _write_cache(self, line: CacheLine, offset_bits: str, page_index: int):
+        if self._write_policy == "write_through":
+            write_through(self._memory, line, offset_bits, page_index)
+        elif self._write_policy == "write_back":
+            write_back(line, offset_bits)
